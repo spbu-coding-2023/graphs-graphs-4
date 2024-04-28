942b914ca3d967ce2a4eabd72d235a2fc5248fdc
Sofya Grishkova <sofagriskova1@gmail.com>
2024-04-28 16:06:42 +0300

    ci: detekt SARIF report upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
 	kotlin("jvm") version "1.9.23"
 	id("io.gitlab.arturbosch.detekt").version("1.23.6")
@@ -25,11 +27,20 @@ kotlin {
 detekt {
 	toolVersion = "1.23.6"
 
-	source.setFrom("src/main/kotlin")
+	source.setFrom("$projectDir/src/main/kotlin", "$projectDir/src/test/kotlin")
 
-	config.setFrom("config/detekt/detekt.yml")
+	config.setFrom("$projectDir/config/detekt/detekt.yml")
 	buildUponDefaultConfig = true
 	allRules = false
 
 	ignoreFailures = false
+
+	basePath = rootProject.projectDir.absolutePath
+}
+
+tasks.withType<Detekt>().configureEach {
+	reports {
+		html.required.set(true) // observe findings in your browser with structure and code snippets
+		sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with GitHub Code Scanning
+	}
 }
