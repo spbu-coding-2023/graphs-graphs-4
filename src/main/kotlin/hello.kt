dceacba6b9da06b7889bacf05178e9c71ec48ec1
Sofya Grishkova <sofagriskova1@gmail.com>
2024-04-28 14:40:40 +0300

    test(ci): detekt workflow fails on exception

---
+++ src/main/kotlin/hello.kt
@@ -0,0 +1,28 @@
+class WORD {
+	var word = "Hello World!"
+
+	fun hello1() {
+		println(1)
+	}
+
+	fun hello9() {
+		println(4)
+	}
+
+	fun hello3() {
+		println(4)
+	}
+
+	fun hello4() {
+		println(4)
+	}
+
+	fun hello5() {
+		println(5)
+	}
+}
+
+fun main(){
+	var write = WORD().word
+	println(write)
+}
