09a4d24f361dfec1a1c2466a73520fc821cd1d70
Sofya Grishkova <sofagriskova1@gmail.com>
2024-04-28 14:05:47 +0300

    сi: detekt setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	kotlin("jvm") version "1.9.23"
+	id("io.gitlab.arturbosch.detekt").version("1.23.6")
 }
 
 group = "org.example"
@@ -16,6 +17,19 @@ dependencies {
 tasks.test {
 	useJUnitPlatform()
 }
+
 kotlin {
 	jvmToolchain(21)
+}
+
+detekt {
+	toolVersion = "1.23.6"
+
+	source.setFrom("src/main/kotlin")
+
+	config.setFrom("config/detekt/detekt.yml")
+	buildUponDefaultConfig = true
+	allRules = false
+
+	ignoreFailures = false
 }
