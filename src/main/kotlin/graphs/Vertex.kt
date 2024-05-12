76954c606c5980761cbcb0407673ca0d2165b5fb
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-05-12 14:16:29 +0300

    feat: add fields in Vertex for Tarjan algorithm

--- src/main/kotlin/graphs/Vertex.kt
+++ src/main/kotlin/graphs/Vertex.kt
@@ -1,6 +1,11 @@
 package graphs
 
-class Vertex<T>(val key: T) {
+class Vertex<T>(
+    val key: T,
+    var sccIndex: Int = 0,
+    var lowLink: Int = 0,
+    var onStack: Boolean = false,
+    ) {
 	// don't forget getter / setter etc.
 	// store coordinates here?
 }
