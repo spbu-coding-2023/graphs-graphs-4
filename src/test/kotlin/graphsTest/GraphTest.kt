13b2d9e9dcd86727fed12b50a15bf4742780385a
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-14 09:49:48 +0300

    build: replace graph tests into a different package

--- src/test/kotlin/graphsTest/GraphTest.kt
+++ src/test/kotlin/graphsTest/GraphTest.kt
@@ -1,5 +1,7 @@
-package graphs
+package graphsTest
 
+import graphs.Graph
+import graphs.Vertex
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
