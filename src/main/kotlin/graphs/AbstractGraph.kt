6f364468cd0bff711815cdac017ba6e6b2b19c12
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-15 18:43:17 +0300

    build: AbstractGraph

---
+++ src/main/kotlin/graphs/AbstractGraph.kt
@@ -0,0 +1,32 @@
+package graphs
+
+import interfaces.Traversable
+
+abstract class AbstractGraph<GRAPH_TYPE, T> : Iterable<Vertex<T>> {
+	internal open var adjList: HashMap<Vertex<T>, HashSet<GRAPH_TYPE>> = HashMap()
+
+	// стоит подумать а нельзя ли быстрее проверять на наличие узла
+	// с каким-то ключем
+	fun addVertex(key: T): Vertex<T> {
+		for (v in adjList.keys) {
+			if (v.key == key) {
+				return v
+			}
+		}
+
+		val vertex = Vertex(key)
+		adjList.putIfAbsent(vertex, HashSet())
+
+		return vertex
+	}
+
+	// Get the vertices adjacent to a given vertex
+	// need to test
+	fun giveNeighbors(vertex: Vertex<T>): Set<GRAPH_TYPE>? {
+		return adjList[vertex]
+	}
+
+	override fun iterator(): Iterator<Vertex<T>> {
+		return this.adjList.keys.iterator()
+	}
+}
