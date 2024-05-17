77d7abadcdb29a89e8a28da40a3366b9a185d2e9
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-17 21:38:02 +0300

    build: create class of directed weighted graph

---
+++ src/main/kotlin/graphs/DirectedWeightedGraph.kt
@@ -0,0 +1,10 @@
+package graphs
+
+class DirectedWeightedGraph<T, NUMBER_TYPE : Number> : WeightedGraph<T, NUMBER_TYPE>() {
+	override  fun addEdge(vertex1: Vertex<T>, vertex2: Vertex<T>, weight: NUMBER_TYPE) {
+		require(adjList.containsKey(vertex1))
+		require(adjList.containsKey(vertex2))
+
+		adjList.getOrPut(vertex1) { HashSet() }.add(Pair(vertex2, weight))
+	}
+}
