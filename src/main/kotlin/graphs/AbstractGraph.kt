86735eb7cb47c04765ad24dc89b41168e226031d
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-17 19:23:15 +0300

    chore: code cleanup

---
+++ src/main/kotlin/graphs/AbstractGraph.kt
@@ -0,0 +1,32 @@
+package graphs
+
+abstract class AbstractGraph<GRAPH_TYPE, T> : Iterable<Vertex<T>> {
+	internal open var adjList: HashMap<Vertex<T>, HashSet<GRAPH_TYPE>> = HashMap()
+
+	var size: Int = 0
+		internal set
+
+	fun addVertex(key: T): Vertex<T> {
+		for (v in adjList.keys) {
+			if (v.key == key) {
+				return v
+			}
+		}
+
+		val vertex = Vertex(key)
+		adjList.putIfAbsent(vertex, HashSet())
+
+		size += 1
+
+		return vertex
+	}
+
+	// need to test
+	fun giveNeighbors(vertex: Vertex<T>): Set<GRAPH_TYPE>? {
+		return adjList[vertex]
+	}
+
+	override fun iterator(): Iterator<Vertex<T>> {
+		return this.adjList.keys.iterator()
+	}
+}
