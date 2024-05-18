9f52b74ed5bc71224b5f0a5e948d4461489b76d1
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-05-18 22:15:28 +0300

    feat: add new class WeightedEdge

---
+++ src/main/kotlin/graphs/WeightedEdge.kt
@@ -0,0 +1,7 @@
+package graphs
+
+data class WeightedEdge<T>(val vertex: Vertex<T>, val weight: Long): Comparable<WeightedEdge<T>> {
+    override fun compareTo(other: WeightedEdge<T>): Int {
+        return (this.weight - other.weight).toInt()
+    }
+}
