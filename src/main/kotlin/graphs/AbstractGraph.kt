d7d925795a92b374f63a890e946681dff15abd48
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-19 14:16:26 +0300

    refactor (abstract graph): move some methods from UndirectedGraph to AbstractGraph

--- src/main/kotlin/graphs/AbstractGraph.kt
+++ src/main/kotlin/graphs/AbstractGraph.kt
@@ -1,10 +1,11 @@
 package graphs
 
 abstract class AbstractGraph<GRAPH_TYPE, T> : Iterable<Vertex<T>> {
-	internal open var adjList: HashMap<Vertex<T>, HashSet<GRAPH_TYPE>> = HashMap()
+	open var adjList: HashMap<Vertex<T>, HashSet<GRAPH_TYPE>> = HashMap()
+		protected set
 
-    var size: Int = 0
-        internal set
+	var size: Int = 0
+        protected set
 
 	fun addVertex(key: T): Vertex<T> {
 		for (v in adjList.keys) {
@@ -14,14 +15,34 @@ abstract class AbstractGraph<GRAPH_TYPE, T> : Iterable<Vertex<T>> {
 		}
 
 		val vertex = Vertex(key)
-		adjList.putIfAbsent(vertex, HashSet())
+		adjList[vertex] = HashSet()
 
 		size += 1
 
 		return vertex
 	}
 
-	// надо ли оно нам?
+	fun addVertex(vertex: Vertex<T>): Vertex<T> {
+		if (adjList.containsKey(vertex)) {
+			return vertex
+		}
+
+		adjList[vertex] = HashSet()
+
+		size += 1
+
+		return vertex
+	}
+
+	// need to test
+	fun removeEdge(vertex1: Vertex<T>, vertex2: Vertex<T>) {
+		require(adjList.containsKey(vertex1))
+		require(adjList.containsKey(vertex2))
+
+		adjList[vertex1]?.remove<Any?>(vertex2)
+		adjList[vertex2]?.remove<Any?>(vertex1)
+	}
+
 	// just converts graph to a set of vertices
 	internal fun convertToVerticesSet(): Set<Vertex<T>> {
 		return adjList.keys
