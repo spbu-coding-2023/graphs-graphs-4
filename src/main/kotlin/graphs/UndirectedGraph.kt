cd510e1f672dab7788f6790b25c5cabe47d19cb8
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-21 22:31:58 +0300

    refactor: code cleanup

--- src/main/kotlin/graphs/UndirectedGraph.kt
+++ src/main/kotlin/graphs/UndirectedGraph.kt
@@ -14,35 +14,6 @@ class UndirectedGraph<T> : AbstractGraph<Vertex<T>, T>() {
 		adjList.getOrPut(vertex2) { HashSet() }.add(vertex1)
 	}
 
-	// need to test
-	fun removeEdge(vertex1: Vertex<T>, vertex2: Vertex<T>) {
-		adjList[vertex1]?.remove(vertex2)
-		adjList[vertex2]?.remove(vertex1)
-	}
-
-	// need to test
-	fun removeVertex(vertex: Vertex<T>) {
-		if (adjList[vertex] != null) {
-			adjList[vertex]?.forEach {
-				adjList[it]?.remove(vertex)
-			}
-
-			adjList.remove(vertex)
-		}
-
-		size -= 1
-	}
-
-	// test on disconnected graph?
-	fun dfsIterator(vertex: Vertex<T>): Iterator<Vertex<T>> {
-		return this.dfs(vertex).iterator()
-	}
-
-	// test?
-	internal fun dfs(vertex: Vertex<T>): Set<Vertex<T>> {
-		return Traversable<T>().dfsIter(this, vertex)
-	}
-
 	fun findBridges(): Set<Pair<Vertex<T>, Vertex<T>>> {
 		return BridgeFinder<T>().findBridges(this)
 	}
