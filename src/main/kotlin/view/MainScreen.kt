fb81f002486e006367b55425502d4cad7749b9f5
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-23 02:47:39 +0300

    feat (gui): add top app bar

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -1,58 +1,61 @@
 package view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import view.graphs.GraphView
 import viewmodel.MainScreenViewModel
 
+
 @Composable
 fun <GRAPH_TYPE, T> MainScreen(viewModel: MainScreenViewModel<GRAPH_TYPE, T>) {
-	Row(
-		horizontalArrangement = Arrangement.spacedBy(20.dp)
-	) {
-		Column(modifier = Modifier.width(370.dp)) {
-			Row {
-				Checkbox(checked = viewModel.showVerticesLabels.value, onCheckedChange = {
-					viewModel.showVerticesLabels.value = it
-				})
-				Text("Show vertices labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
-			}
-			Row {
-				Checkbox(checked = viewModel.showEdgesLabels.value, onCheckedChange = {
-					viewModel.showEdgesLabels.value = it
-				})
-				Text("Show edges labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
-			}
-			Button(
-				onClick = viewModel::resetGraphView,
-				enabled = true,
-			) {
-				Text(
-					text = "Reset default settings",
-				)
-			}
-			Button(
-				onClick = viewModel::setVerticesColor,
-				enabled = true,
-			) {
-				Text(
-					text = "Set colors",
-				)
-			}
-		}
+	val drawerState = rememberDrawerState(DrawerValue.Closed)
+	val scope = rememberCoroutineScope()
+	val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+	var expanded by remember { mutableStateOf(false) }
 
-		Surface(
-			modifier = Modifier.weight(1f),
+	Column(modifier = Modifier.fillMaxSize()) {
+		Scaffold(
+			scaffoldState = scaffoldState,
+
+			topBar = {
+				TopAppBar(
+					title = { Text("Graph Editor") },
+
+					navigationIcon = {
+						IconButton(onClick = { expanded = true }) {
+							Icon(Icons.Filled.Menu, contentDescription = "Main Menu")
+						}
+					}
+				)
+			}
 		) {
-			GraphView(viewModel.graphViewModel)
-		}
+			Surface(
+				modifier = Modifier.weight(1f),
+			) {
+				GraphView(viewModel.graphViewModel)
+			}
 
+			Column {
+				Row {
+					Checkbox(checked = viewModel.showVerticesLabels.value, onCheckedChange = {
+						viewModel.showVerticesLabels.value = it
+					})
+					Text("Show vertices labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
+				}
+
+				Row {
+					Checkbox(checked = viewModel.showEdgesLabels.value, onCheckedChange = {
+						viewModel.showEdgesLabels.value = it
+					})
+					Text("Show edges labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
+				}
+			}
+		}
 	}
 }
