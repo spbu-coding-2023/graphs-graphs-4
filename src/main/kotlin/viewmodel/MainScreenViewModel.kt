53d7d0afbaf215e242f03dcae644630916e4db70
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-22 12:54:41 +0300

    build: draft on gui

---
+++ src/main/kotlin/viewmodel/MainScreenViewModel.kt
@@ -0,0 +1,29 @@
+package viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
+import model.graphs.AbstractGraph
+import viewmodel.graphs.GraphViewModel
+import viewmodel.graphs.RepresentationStrategy
+
+class MainScreenViewModel<GRAPH_TYPE, T> (
+	graph: AbstractGraph<GRAPH_TYPE, T>,
+	private val representationStrategy: RepresentationStrategy)
+{
+	val showVerticesLabels = mutableStateOf(false)
+	val showEdgesLabels = mutableStateOf(false)
+	val graphViewModel = GraphViewModel(graph, showVerticesLabels, showEdgesLabels)
+
+	init {
+		representationStrategy.place(800.0, 600.0, graphViewModel.vertices)
+	}
+
+	fun resetGraphView() {
+		representationStrategy.place(800.0, 600.0, graphViewModel.vertices)
+		graphViewModel.vertices.forEach { v -> v.color = Color.Gray }
+	}
+
+	fun setVerticesColor() {
+		representationStrategy.highlight(graphViewModel.vertices)
+	}
+}
---
+++ src/main/kotlin/viewmodel/graphs/GraphViewModel.kt
@@ -0,0 +1,30 @@
+package viewmodel.graphs
+
+import androidx.compose.runtime.State
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import model.graphs.AbstractGraph
+
+class GraphViewModel<GRAPH_TYPE, T>(
+    private val graph: AbstractGraph<GRAPH_TYPE, T>,
+    showVerticesLabels: State<Boolean>,
+    showEdgesLabels: State<Boolean>,
+) {
+	private val _vertices = graph.adjList.keys.associateWith { v ->
+		VertexViewModel(0.dp, 0.dp, Color.Gray, v, showVerticesLabels)
+	}
+
+//	private val _edges = graph.adjList.values.associateWith { e ->
+//		val fst = _vertices[e.vertices.first]
+//			?: throw IllegalStateException("VertexView for ${e.vertices.first} not found")
+//		val snd = _vertices[e.vertices.second]
+//			?: throw IllegalStateException("VertexView for ${e.vertices.second} not found")
+//		EdgeViewModel(fst, snd, e, showEdgesLabels)
+//	}
+
+	val vertices: Collection<VertexViewModel<T>>
+		get() = _vertices.values
+
+//	val edges: Collection<EdgeViewModel<E, V>>
+//		get() = _edges.values
+}
