4d2421604156860ae9a404336c778e323a5c0aaa
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-23 10:48:42 +0300

    feat (gui): tools panel

--- src/main/kotlin/view/MainScreen.kt
+++ src/main/kotlin/view/MainScreen.kt
@@ -1,15 +1,13 @@
 package view
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import view.graphs.GraphView
 import viewmodel.MainScreenViewModel
 
@@ -22,10 +20,12 @@ fun <GRAPH_TYPE, T> MainScreen(viewModel: MainScreenViewModel<GRAPH_TYPE, T>) {
 			topBar = {
 				TopAppBar(
 					title = { Text("Graph Application") },
+
 					navigationIcon = {
 						IconButton(onClick = { showMenu = true }) {
 							Icon(Icons.Filled.Menu, contentDescription = "Menu")
 						}
+
 						DropdownMenu(
 							expanded = showMenu,
 							onDismissRequest = { showMenu = false }
@@ -33,13 +33,17 @@ fun <GRAPH_TYPE, T> MainScreen(viewModel: MainScreenViewModel<GRAPH_TYPE, T>) {
 							DropdownMenuItem(onClick = { /* код */ }) {
 								Text("New Graph")
 							}
+
 							DropdownMenuItem(onClick = { /* код */ }) {
 								Text("Open Graph")
 							}
+
 							DropdownMenuItem(onClick = { /* код */ }) {
 								Text("Save Graph")
 							}
+
 							Divider()
+
 							DropdownMenuItem(onClick = { /* код */ }) {
 								Text("Exit")
 							}
@@ -48,44 +52,47 @@ fun <GRAPH_TYPE, T> MainScreen(viewModel: MainScreenViewModel<GRAPH_TYPE, T>) {
 				)
 			}
 		) {
-			Column(modifier = Modifier.fillMaxSize()) {
-				Surface(
-					modifier = Modifier.weight(1f),
-				) {
+			Row(
+				horizontalArrangement = Arrangement.spacedBy(20.dp)
+			) {
+				Column(modifier = Modifier.width(370.dp)) {
+					ToolsPanel(
+						modifier = Modifier.weight(1f).background(MaterialTheme.colors.secondary),
+						viewModel = viewModel
+					)
+				}
+
+				Surface(modifier = Modifier.weight(1f)) {
 					GraphView(viewModel.graphViewModel)
 				}
 			}
-
-//			Column {
-//				Row {
-//					Checkbox(checked = viewModel.showVerticesLabels.value, onCheckedChange = {
-//						viewModel.showVerticesLabels.value = it
-//					})
-//					Text("Show vertices labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
-//				}
-//
-//				Row {
-//					Checkbox(checked = viewModel.showEdgesLabels.value, onCheckedChange = {
-//						viewModel.showEdgesLabels.value = it
-//					})
-//					Text("Show edges labels", fontSize = 28.sp, modifier = Modifier.padding(4.dp))
-//				}
-//			}
 		}
 	}
 }
 
 
 @Composable
-fun <GRAPH_TYPE, T> MainRow(
+fun <GRAPH_TYPE, T> ToolsPanel(
 	viewModel: MainScreenViewModel<GRAPH_TYPE, T>,
-	modifier: Modifier
+	modifier: Modifier = Modifier
 ) {
-	Row(
-		modifier = modifier.padding(16.dp),
-		verticalAlignment = Alignment.CenterVertically
+	Column(
+		modifier = modifier
+			.fillMaxHeight()
+			.padding(16.dp)
 	) {
+		Button(
+			onClick = viewModel::resetGraphView,
+			enabled = true,
+		) {
+			Text(text = "Reset default settings")
+		}
 
+		Button(
+			onClick = viewModel::setVerticesColor,
+			enabled = true,
+		) {
+			Text(text = "Set colors")
+		}
 	}
-
 }
