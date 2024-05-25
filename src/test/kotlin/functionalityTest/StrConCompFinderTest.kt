14d7d8aa8a86e05317a8420eff6ab8c65730b09f
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-05-25 22:06:15 +0300

    feat: add tests for scc finder

---
+++ src/test/kotlin/functionalityTest/StrConCompFinderTest.kt
@@ -0,0 +1,84 @@
+package functionalityTest
+
+import model.graphs.DirectedGraph
+import model.graphs.Edge
+import model.graphs.Vertex
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import kotlin.math.exp
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StrConCompFinderTest {
+    private lateinit var graphInt: DirectedGraph<Int>
+    private lateinit var expectedSCC: MutableSet<Set<Vertex<Int>>>
+
+    @BeforeEach
+    fun clear() {
+        graphInt = DirectedGraph()
+        expectedSCC = mutableSetOf()
+    }
+
+    @Test
+    @DisplayName("Max-edged graph.")
+    fun sccTest1() {
+        for (key in 0..5) {
+            graphInt.addVertex(key)
+        }
+
+        for (vertex1 in 0..5) {
+            for (vertex2 in 0..5) {
+                if (vertex1 != vertex2) {
+                    graphInt.addEdge(vertex1, vertex2)
+                }
+            }
+        }
+
+        val component = graphInt.vertices()
+        expectedSCC.add(component)
+
+        assertEquals(expectedSCC, graphInt.findSCC())
+    }
+
+    @Test
+    @DisplayName("Zero-edged graph.")
+    fun sccTest2() {
+        for (key in 0..5) {
+            graphInt.addVertex(key)
+        }
+
+        for (key in 0..5) {
+            val component = setOf(Vertex(key))
+            expectedSCC.add(component)
+        }
+
+        assertEquals(expectedSCC, graphInt.findSCC())
+    }
+
+    @Test
+    @DisplayName("3 strong connected components without edges between them.")
+    fun sccTest3() {
+        val vertices = Array(8) { Vertex(it) }
+        val edges = arrayOf(
+            Edge(0, 1),
+            Edge(1, 0),
+            Edge(2, 3),
+            Edge(3, 4),
+            Edge(4, 2),
+            Edge(5, 6),
+            Edge(6, 7),
+            Edge(7, 6),
+            Edge(7, 5),
+            )
+
+        graphInt.addVertices(*vertices)
+        graphInt.addEdges(*edges)
+        expectedSCC = mutableSetOf(
+            setOf(Vertex(0), Vertex(1)),
+            setOf(Vertex(2), Vertex(3), Vertex(4)),
+            setOf(Vertex(5), Vertex(6), Vertex(7)),
+            )
+
+        assertEquals(expectedSCC, graphInt.findSCC())
+    }
+}
