cb134822a5a6357dfc633789cb62ac1f4019c29f
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-05-26 11:12:52 +0300

    feat: add methods equals, hashCode, compareTo

--- src/main/kotlin/model/graphs/Edge.kt
+++ src/main/kotlin/model/graphs/Edge.kt
@@ -1,6 +1,29 @@
 package model.graphs
 
-open class Edge<K>(
-    open val from: K,
-    open val to: K,
-    )
+import java.util.*
+
+class Edge<K>(
+    val from: K,
+    val to: K,
+    ): Comparable<Edge<K>>  {
+    override fun toString(): String {
+        return "($from, $to)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is Edge<*> &&
+            ((from == other.from && to == other.to) ||
+                (from == other.to && to == other.from))
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(from, to)
+    }
+
+    override fun compareTo(other: Edge<K>): Int {
+        return when {
+            from == other.from -> to.hashCode().compareTo(other.to.hashCode())
+            else -> from.hashCode().compareTo(other.from.hashCode())
+        }
+    }
+    }
