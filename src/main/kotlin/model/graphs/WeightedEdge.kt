e5f35a13d2760050d6db07215fd2fd7904b14e00
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-05-26 11:12:52 +0300

    feat: add methods equals, hashCode, compareTo

--- src/main/kotlin/model/graphs/WeightedEdge.kt
+++ src/main/kotlin/model/graphs/WeightedEdge.kt
@@ -1,22 +1,53 @@
 package model.graphs
 
+import java.util.*
+
 data class WeightedEdge<K, W: Number>(
-	override val from: K,
-	override val to: K,
+	val from: K,
+	val to: K,
 	val weight: W,
-	) : Comparable<WeightedEdge<K, W>>, Edge<K>(from, to) {
+	) : Comparable<WeightedEdge<K, W>> {
 	operator fun Number.minus(other: Number): Number {
 		return when (this) {
-			is Long -> this + other.toLong()
-			is Int -> this + other.toLong()
-			is Short -> this + other.toLong()
-			is Double -> this + other.toDouble()
-			is Float -> this + other.toDouble()
+			is Long -> this - other.toLong()
+			is Int -> this - other.toLong()
+			is Short -> this - other.toLong()
+			is Double -> this - other.toDouble()
+			is Float -> this - other.toDouble()
 			else -> throw IllegalArgumentException("Unknown numeric type")
 		}
 	}
 
+	operator fun Number.compareTo(other: Number): Int {
+		return when (this) {
+			is Long -> this.compareTo(other.toLong())
+			is Int -> this.compareTo(other.toLong())
+			is Short -> this.compareTo(other.toLong())
+			is Double -> this.compareTo(other.toDouble())
+			is Float -> this.compareTo(other.toDouble())
+			else -> throw IllegalArgumentException("Unknown numeric type")
+		}
+	}
+
+	override fun toString(): String {
+		return "($from,$to|$weight)"
+	}
+
+	override fun equals(other: Any?): Boolean {
+		return other is WeightedEdge<*, *> && (weight == other.weight) &&
+			((from == other.from && to == other.to) ||
+				(from == other.to && to == other.from))
+	}
+
+	override fun hashCode(): Int {
+		return Objects.hash(from, to, weight)
+	}
+
 	override fun compareTo(other: WeightedEdge<K, W>): Int {
-		return (weight - other.weight).toInt()
+		return when {
+			weight == other.weight && from == other.from -> to.hashCode().compareTo(other.to.hashCode())
+			weight == other.weight -> from.hashCode().compareTo(other.from.hashCode())
+			else -> weight.compareTo(other.weight)
+		}
 	}
 }
