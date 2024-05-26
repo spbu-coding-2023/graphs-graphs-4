e65bfd8006c9bbd0fc195b067c8aa9699792c77e
Sofya Grishkova <sofagriskova1@gmail.com>
2024-05-26 15:50:41 +0300

    refactor: cleanup interface Graph

--- src/main/kotlin/model/graphs/DirectedGraph.kt
+++ src/main/kotlin/model/graphs/DirectedGraph.kt
@@ -1,6 +1,7 @@
 package model.graphs
 
 class DirectedGraph<T> : UndirectedGraph<T>() {
+
 	override fun addEdge(vertex1: Vertex<T>, vertex2: Vertex<T>) {
 		require(adjList.containsKey(vertex1))
 		require(adjList.containsKey(vertex2))
--- src/main/kotlin/model/graphs/Graph.kt
+++ src/main/kotlin/model/graphs/Graph.kt
@@ -1,106 +1,17 @@
 package model.graphs
 
 interface Graph<GRAPH_TYPE, T> : Iterable<Vertex<T>> {
+	fun addVertex(key: T): Vertex<T>
 
-	var adjList: HashMap<Vertex<T>, HashSet<GRAPH_TYPE>>
+	fun addVertex(vertex: Vertex<T>): Vertex<T>
 
-	var size: Int
+	fun addVertices(vararg keys: T)
 
-	fun addVertex(key: T): Vertex<T> {
-		for (v in adjList.keys) {
-			if (v.key == key) {
-				return v
-			}
-		}
+	fun addVertices(vararg vertices: Vertex<T>)
 
-		val vertex = Vertex(key)
-		adjList[vertex] = HashSet()
+	fun vertices(): Set<Vertex<T>>
 
-		size += 1
+	override fun iterator(): Iterator<Vertex<T>>
 
-		return vertex
-	}
-
-	fun addVertex(vertex: Vertex<T>): Vertex<T> {
-		if (adjList.containsKey(vertex)) {
-			return vertex
-		}
-
-		adjList[vertex] = HashSet()
-
-		size += 1
-
-		return vertex
-	}
-
-	fun addVertices(vararg keys: T): Unit {
-		for (key in keys) {
-			addVertex(key)
-		}
-	}
-
-	fun addVertices(vararg vertices: Vertex<T>): Unit {
-		for (vertex in vertices) {
-			addVertex(vertex)
-		}
-	}
-
-	// need to test
-	//Странно, что добавлять узлы в абстрактном графе нельзя, а удалять - можно.
-	//Причём он неверно будет работать для ориентированного графа.
-	fun removeEdge(vertex1: Vertex<T>, vertex2: Vertex<T>) {
-		require(adjList.containsKey(vertex1))
-		require(adjList.containsKey(vertex2))
-
-		adjList[vertex1]?.remove<Any?>(vertex2)
-		adjList[vertex2]?.remove<Any?>(vertex1)
-	}
-
-	fun <E: Edge<T>>removeEdge(edge: E ) {
-		val vertex1 = Vertex(edge.from)
-		val vertex2 = Vertex(edge.to)
-
-		return removeEdge(vertex1, vertex2)
-	}
-
-	// need to test AND generalise
-//	fun removeVertex(vertex: Vertex<T>) {
-//		if (adjList[vertex] != null) {
-//			adjList[vertex]?.forEach {
-//				adjList[it]?.remove(vertex)
-//			}
-//
-//			adjList.remove(vertex)
-//		}
-//
-//		size -= 1
-//	}
-
-	// just converts graph to a set of vertices
-	fun vertices(): Set<Vertex<T>> {
-		return adjList.keys
-	}
-
-	//	fun dfsIterator(vertex: Vertex<T>): Iterator<Vertex<T>> {
-//		return this.dfs(vertex).iterator()
-//	}
-//
-//	// test?
-//	internal fun dfs(vertex: Vertex<T>): Set<Vertex<T>> {
-//		return Traversable<T>().dfsIter(this, vertex)
-//	}
-	// need to test
-	override fun iterator(): Iterator<Vertex<T>> {
-		return this.adjList.keys.iterator()
-	}
-
-	fun getNeighbors(vertex: Vertex<T>): HashSet<GRAPH_TYPE> {
-		return adjList[vertex] ?: throw IllegalArgumentException(
-			"Can't get neighbors for vertex $vertex that is not in the graph"
-		)
-	}
-
-	fun getNeighbors(key: T): HashSet<GRAPH_TYPE> {
-		return getNeighbors(Vertex(key))
-	}
+	fun getNeighbors(vertex: Vertex<T>): HashSet<GRAPH_TYPE>
 }
