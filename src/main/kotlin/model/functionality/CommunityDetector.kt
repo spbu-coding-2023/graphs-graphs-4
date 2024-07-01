bd23edea90d44a77859578db13d24607a20f54e3
Sofya Grishkova <sofagriskova1@gmail.com>
2024-07-01 16:46:34 +0300

    feat (community detection): merge nodes fast fun

--- src/main/kotlin/model/functionality/CommunityDetector.kt
+++ src/main/kotlin/model/functionality/CommunityDetector.kt
@@ -5,8 +5,17 @@ import model.graphs.UndirectedGraph
 import model.graphs.Vertex
 
 class CommunityDetector<T>(var graph: GraphUndirected<T>, var y: Double) {
-    private fun flatten(partition: HashSet<HashSet<Vertex<T>>>) {
-        TODO()
+    /// еще ОЧЕНЬ наверное стоит дробить T пока не избавимся от сета
+
+
+    private fun flatten(partition: HashSet<HashSet<Vertex<T>>>): HashSet<HashSet<*>> {
+        val output = HashSet<HashSet<*>>()
+
+        for (community in partition) {
+            output.add(flatCommunity(community))
+        }
+
+        return output
     }
 
     fun leiden() {
@@ -79,7 +88,7 @@ class CommunityDetector<T>(var graph: GraphUndirected<T>, var y: Double) {
         return sum
     }
 
-    private fun countEdges(graph: GraphUndirected<T>, set1: HashSet<Vertex<T>>, set2: HashSet<Vertex<T>>): Int {
+    private fun countEdges(graph: GraphUndirected<T>, set1: HashSet<Vertex<T>>, set2: Set<Vertex<T>>): Int {
         var count = 0
 
         for (u in set1) {
@@ -138,14 +147,89 @@ class CommunityDetector<T>(var graph: GraphUndirected<T>, var y: Double) {
         return refinedPartition
     }
 
+    private fun countEdgesInside(graph: GraphUndirected<T>, set: HashSet<Vertex<T>>, vertex: Vertex<T>): Int {
+        var count = 0
+
+        for (neighbor in graph.getNeighbors(vertex)) {
+            if (set.contains(neighbor.to)) {
+                count += 1
+            }
+        }
+
+        return count
+    }
+
+    private fun flatVertex(vertex: Vertex<T>): HashSet<*> {
+        if (vertex.key is HashSet<*>) {
+            return vertex.key
+        } else {
+            return hashSetOf(vertex)
+        }
+    }
+
+    private fun flatCommunity(community: HashSet<Vertex<T>>): HashSet<*> {
+        val output: HashSet<Vertex<T>> = hashSetOf()
+
+        for (vertex in community) {
+            for (element in flatVertex(vertex)) {
+                output.add(element as Vertex<T>)
+            }
+        }
+
+        return output
+    }
+
     private fun mergeNodesSubset(
         graph: GraphUndirected<T>,
         refinedPartition: HashSet<HashSet<Vertex<T>>>,
-        community: java.util.HashSet<Vertex<T>>
+        community: HashSet<Vertex<T>>
     ): HashSet<HashSet<Vertex<T>>> {
+        val r: HashSet<Vertex<T>> = hashSetOf()
 
+        for (vertex in community) {
+            val vertexSize = flatVertex(vertex).size
 
-        TODO()
+            if (countEdgesInside(
+                    graph,
+                    community,
+                    vertex
+                ) >= (y * vertexSize * (flatCommunity(community).size - vertexSize))
+            ) {
+                r.add(vertex)
+            }
+        }
+
+        for (vertex in r) {
+            if (refinedPartition.find { it.contains(vertex) }?.size == 1) {
+                val wellConnectedCommunities = hashSetOf<HashSet<Vertex<T>>>()
+
+                // мб функцию ?
+                for (element in refinedPartition) {
+                    if (flatCommunity(element).all { community.contains(it) }) {
+                        if (countEdges(
+                                graph,
+                                element,
+                                community.minus(element)
+                            ) > (y * flatCommunity(element).size * (flatCommunity(community).size - flatCommunity(
+                                element
+                            ).size))
+                        ) {
+                            wellConnectedCommunities.add(element)
+                        }
+                    }
+                }
+
+                // была какая-то формула но написано вернуть случайное
+                // вообще по хорошему надо проверять как изменилось качество
+                val newCommunity = refinedPartition.random()
+
+                refinedPartition.find { it.contains(vertex) }?.remove(vertex)
+
+                newCommunity.add(vertex)
+            }
+        }
+
+        return refinedPartition
     }
 
     private fun initPartition(graph: GraphUndirected<T>): HashSet<HashSet<Vertex<T>>> {
