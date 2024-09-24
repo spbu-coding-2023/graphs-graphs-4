cf50198008b00203aea9679f47bdeb83ea30ebef
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-09-24 17:51:10 +0300

    chore: add commentaries

--- src/main/kotlin/model/functionality/MinSpanTreeFinder.kt
+++ src/main/kotlin/model/functionality/MinSpanTreeFinder.kt
@@ -7,11 +7,12 @@ import model.graphs.UndirectedGraph
 class MinSpanTreeFinder<T, E: Edge<T>>(private val graph: GraphUndirected<T, E>) {
     fun mstSearch(): Set<Edge<T>> {
         val spanningTree = UndirectedGraph<T>()
-
+        //step 1: add any vertex in spanning tree
         val firstVertex = graph.firstOrNull() ?: return spanningTree.edges()
         spanningTree.addVertex(firstVertex)
 
         while (spanningTree.size != graph.size) {
+            //step 2: search edge that connects different connection components
             var minEdge: Edge<T>? = null
 
             for (vertex in spanningTree) {
@@ -26,6 +27,7 @@ class MinSpanTreeFinder<T, E: Edge<T>>(private val graph: GraphUndirected<T, E>)
                 }
             }
 
+            //step 3: add this edge and adjacent vertex in spanning tree
             if (minEdge != null) {
                 val minEdgeVertex = minEdge.to
                 spanningTree.addVertex(minEdgeVertex)
