95e814bf7d37e8981a5f075ab62bb42d24d2e95c
Damir Yunusov <kglwkglwgklw@gmail.com>
2024-09-24 17:48:07 +0300

    refactor: remove inside function

--- src/main/kotlin/model/functionality/MinSpanTreeFinder.kt
+++ src/main/kotlin/model/functionality/MinSpanTreeFinder.kt
@@ -12,31 +12,25 @@ class MinSpanTreeFinder<T, E: Edge<T>>(private val graph: GraphUndirected<T, E>)
         spanningTree.addVertex(firstVertex)
 
         while (spanningTree.size != graph.size) {
-            fun findEdgeWithMinWeight(): Edge<T>? {
-                var minEdge: Edge<T>? = null
+            var minEdge: Edge<T>? = null
 
-                for (vertex in spanningTree) {
-                    val adjEdges = graph.getNeighbors(vertex)
+            for (vertex in spanningTree) {
+                val adjEdges = graph.getNeighbors(vertex)
 
-                    for (edge in adjEdges) {
-                        if (!spanningTree.contains(edge.to) && minEdge == null) {
-                            minEdge = edge
-                        } else if (spanningTree.contains(edge.to) && minEdge != null) {
-                            if (minEdge > edge) minEdge = edge
-                        }
+                for (edge in adjEdges) {
+                    if (!spanningTree.contains(edge.to) && minEdge == null) {
+                        minEdge = edge
+                    } else if (spanningTree.contains(edge.to) && minEdge != null) {
+                        if (minEdge > edge) minEdge = edge
                     }
                 }
-
-                return minEdge
             }
-
-            val minEdge = findEdgeWithMinWeight()
 
             if (minEdge != null) {
                 val minEdgeVertex = minEdge.to
                 spanningTree.addVertex(minEdgeVertex)
                 spanningTree.addEdge(minEdge.from, minEdge.to)
-            } else {
+            } else { //graph isn't connected
                 return emptySet()
             }
         }
